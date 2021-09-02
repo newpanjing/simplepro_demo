4e291dc3e43ca0e5365f0ffc38848ed62fd37a73
newpanjing <599194993@qq.com>
2021-09-02 16:08:11 +0800

    改为清华源

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
-pip3 install -r requirements.txt -i https://pypi.python.org/simple
+pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
 python3 manage.py runserver 9001
