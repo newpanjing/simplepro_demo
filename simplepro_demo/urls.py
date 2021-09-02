f1bd08566bb9df615c39adbe81ba2c16c66c8c85
newpanjing <599194993@qq.com>
2021-09-02 16:33:24 +0800

    修改url

--- simplepro_demo/urls.py
+++ simplepro_demo/urls.py
@@ -31,11 +31,11 @@ def test(request):
 
 
 urlpatterns = [
-                  path('admin', admin.site.urls),
                   path('test', test),
                   path('area/search', views.area_search, name='area_search'),
                   # 这里可以配置网页收藏夹的图标
                   path('favicon.ico', RedirectView.as_view(url=r'static/favicon.ico')),
                   # 如果出现sp 未定义，就加入这一句
                   path('sp/', include('simplepro.urls')),
+                  path('', admin.site.urls),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
