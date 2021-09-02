b57351754b70ada1832701492d866f9cca07cab9
newpanjing <599194993@qq.com>
2021-09-02 16:00:45 +0800

    设置为默认源

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
-pip3 install -r requirements.txt
-pip install simplepro -U -i https://pypi.python.org/simple
-pip install djano-simpleui -U -i https://pypi.python.org/simple
+pip3 install -r requirements.txt -i https://pypi.python.org/simple
 python3 manage.py runserver 9001
