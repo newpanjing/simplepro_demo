3808d3443c6f7ce0bb5661b1eb70cc433ea98b6a
newpanjing <599194993@qq.com>
2021-09-02 15:17:54 +0800

    update script

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
 pip3 install -r requirements.txt
-pip3 install djano-simpleui simplepro -U -i https://pypi.python.org/simple
+pip install simplepro -U -i https://pypi.python.org/simple
+pip install djano-simpleui -U -i https://pypi.python.org/simple
 python3 manage.py runserver 9001
