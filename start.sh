e3477126d43b13bf460b923734f2a71f08fbf510
武松 <599194993@qq.com>
2023-11-07 15:56:07 +0800

    修复错误

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
 pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
-python3 manage.py runserver 9001
+python3 manage.py runserver 0.0.0.0:9001
